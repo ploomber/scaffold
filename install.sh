808f0c31f30da9e9bb5f5bbe749464e1dd426bdd
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-22 15:05:28 -0500

    install script fixes

--- install.sh
+++ install.sh
@@ -7,8 +7,9 @@ echo -e '\nSource: https://www.python.org/dev/peps/pep-0008/\n'
 
 read -p "Package name: " PROJECT_NAME
 
-mv template/ "$PROJECT_NAME/"
 sed -i '' "s/{{package_name}}/$PROJECT_NAME/g" template/template/setup.py
 mv "template/src/{{package_name}}" "template/src/$PROJECT_NAME"
 
-mv template/* ../*
+mv template/* .
+
+mv template/ "$PROJECT_NAME/"
