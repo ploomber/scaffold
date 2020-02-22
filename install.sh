461678cb27845c5a6e2bec9e7fb8b6e525264879
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-22 14:52:23 -0500

    adds minimal structure

---
+++ install.sh
@@ -0,0 +1,11 @@
+ #!/usr/bin/env bash
+ 
+
+echo "Python packages should also have short, all-lowercase names,
+although the use of underscores is discouraged."
+echo -e '\nSource: https://www.python.org/dev/peps/pep-0008/\n'
+
+read -p "Package name: " PROJECT_NAME
+
+sed -i '' "s/{{package_name}}/$PROJECT_NAME/g" template/setup.py
+mv "template/src/{{package_name}}" "template/src/$PROJECT_NAME"
---
+++ test.sh
@@ -0,0 +1,5 @@
+ conda remove --name test --all --yes
+
+conda create --name test python=3 --yes
+
+conda activate test
