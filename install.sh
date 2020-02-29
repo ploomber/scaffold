51824df71d65df714ea89d548a5029906405182e
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-29 06:52:28 -0500

    install.sh now accepts an argument

--- install.sh
+++ install.sh
@@ -5,7 +5,13 @@ echo "Python packages should also have short, all-lowercase names,
 although the use of underscores is discouraged."
 echo -e '\nSource: https://www.python.org/dev/peps/pep-0008/\n'
 
-read -p "Package name: " PROJECT_NAME
+
+if [ $# -eq 0 ]
+    then
+        read -p "Package name: " PROJECT_NAME
+    else
+        PROJECT_NAME=$1
+fi
 
 sed -i '' "s/{{package_name}}/$PROJECT_NAME/g" template-master/template/setup.py
 sed -i '' "s/{{package_name}}/$PROJECT_NAME/g" template-master/template/README.md
