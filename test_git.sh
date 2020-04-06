c3894ad9bd6ab6ae48f8b1e1535ed7df33c72fb7
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-04-05 23:30:13 -0400

    using hash to test the right commit

--- test_git.sh
+++ test_git.sh
@@ -1,5 +1,15 @@
 set -e
 
+if [[ -z "$1" ]]
+then
+    echo "Missing GIT_HASH argument, retrieving it from the current repo"
+    GIT_HASH=$(git rev-parse HEAD)
+else
+    GIT_HASH="$1"
+fi
+
+echo "Testing git commit $GIT_HASH"
+
 echo 'creating test conda env...'
 conda remove --name test --all --yes
 conda create --name test python=3 --yes
@@ -7,8 +17,13 @@ eval "$(conda shell.bash hook)"
 conda activate test
 
 rm -rf tmp/
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git clone --branch=$BRANCH https://github.com/ploomber/template tmp/
+git clone https://github.com/ploomber/template tmp/
+git checkout $GIT_HASH
 cd tmp/
 python install.py --name my_sample_package
+
+# make sure .git is not deleted
+test -d .git
+
+cd ..
 rm -rf tmp/
