b9b24b23655a1c20ae025cd8f222b7bb5770c6d6
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-04-27 20:58:06 -0400

    fixing pytest call

--- tests/test_setup_sh.py
+++ tests/test_setup_sh.py
@@ -2,18 +2,18 @@ from invoke import run
 
 
 def test_help():
-    result = run('cd template/ && bash setup.sh --help', hide=True)
+    result = run('cd template/ && bash setup.sh --help')
     assert result.ok
 
 
 def test_setup_sh():
-    result = run('cd template/ && bash setup.sh some_env', hide=True)
+    result = run('cd template/ && bash setup.sh some_env')
     assert result.ok
 
     # check env was created
-    result = run('conda env list | grep some_env', hide=True)
+    result = run('conda env list | grep some_env')
     assert 'some_env' in result.stdout
 
     # delete env
-    result = run('conda remove --name some_env --all --yes', hide=True)
+    result = run('conda remove --name some_env --all --yes')
     assert result.ok
