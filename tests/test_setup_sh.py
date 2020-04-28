6440c0de5ac22929a50779229e9ca9395c11fea2
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-04-27 20:51:12 -0400

    hiding output in tests

--- tests/test_setup_sh.py
+++ tests/test_setup_sh.py
@@ -7,13 +7,13 @@ def test_help():
 
 
 def test_setup_sh():
-    result = run('cd template/ && bash setup.sh some_env')
+    result = run('cd template/ && bash setup.sh some_env', hide=True)
     assert result.ok
 
     # check env was created
-    result = run('conda env list | grep some_env')
+    result = run('conda env list | grep some_env', hide=True)
     assert 'some_env' in result.stdout
 
     # delete env
-    result = run('conda remove --name some_env --all --yes')
+    result = run('conda remove --name some_env --all --yes', hide=True)
     assert result.ok
