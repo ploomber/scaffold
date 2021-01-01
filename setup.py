73974b96bdb21a71494be112b399605b3f9b1f00
Eduardo Blancas Reyes <github@blancas.io>
2021-01-01 11:50:55 -0600

    adding pytho pkg structure

---
+++ setup.py
@@ -0,0 +1,62 @@
+import io
+import re
+import ast
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('src/ploomber_scaffold/__init__.py', 'rb') as f:
+    VERSION = str(
+        ast.literal_eval(
+            _version_re.search(f.read().decode('utf-8')).group(1)))
+
+
+def read(*names, **kwargs):
+    return io.open(join(dirname(__file__), *names),
+                   encoding=kwargs.get('encoding', 'utf8')).read()
+
+
+DEV = ['twine', 'flake8', 'yapf', 'pytest', 'invoke']
+
+setup(
+    name='ploomber-scaffold',
+    version=VERSION,
+    description=None,
+    long_description=None,
+    author=None,
+    author_email=None,
+    url='https://github.com/ploomber/ploomber-scaffold',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords=[],
+    install_requires=[],
+    extras_require={
+        'dev': DEV,
+    },
+    entry_points={},
+)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
 import sys
 from pathlib import Path
+# add template/lib to path, we'll be testing those files
 sys.path.append(str(Path(__file__).parent.parent / 'template' / 'lib'))
