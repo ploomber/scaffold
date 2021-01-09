15436c92f684005bd2c4ad48672a2de94b4c8366
Eduardo Blancas Reyes <github@blancas.io>
2021-01-09 08:57:53 -0600

    checking wheel does not contain any files not needed for the template

--- tests/test_scaffold.py
+++ tests/test_scaffold.py
@@ -1,6 +1,9 @@
+import zipfile
 import os
 import subprocess
 from pathlib import Path
+import shutil
+from itertools import chain
 
 from ploomber_scaffold import scaffold
 
@@ -32,7 +35,7 @@ def test_cli(tmp_directory):
 
     assert not subprocess.run(['bash', 'build.sh']).returncode
 
-    # test installing from wheel
+    # test installing project from wheel
     script = """
     eval "$(conda shell.bash hook)"
     conda activate my_new_project
@@ -44,3 +47,38 @@ def test_cli(tmp_directory):
     Path('build_from_wheel.sh').write_text(script)
 
     assert not subprocess.run(['bash', 'build_from_wheel.sh']).returncode
+
+
+def test_scaffold_wheel_contents(tmp_path):
+    """Make sure the wheel does not contain stuff it shouldn't have
+    """
+    if Path('build').exists():
+        shutil.rmtree('build')
+
+    if Path('dist').exists():
+        shutil.rmtree('dist')
+
+    subprocess.run(['python', 'setup.py', 'bdist_wheel'])
+    whl_name = os.listdir('dist')[0]
+
+    with zipfile.ZipFile(Path('dist', whl_name), 'r') as zip_ref:
+        zip_ref.extractall(tmp_path)
+
+    path = Path(tmp_path, 'ploomber_scaffold', 'template')
+
+    assert not (path / 'dist').exists()
+    assert not (path / 'build').exists()
+    assert not (path / '.nox').exists()
+
+    files_and_dirs = chain(
+        *[dirnames + filenames for _, dirnames, filenames in os.walk(path)])
+
+    hidden = [
+        f for f in files_and_dirs if f.startswith('.')
+        if f not in {'.gitkeep', '.gitignore'}
+    ]
+
+    assert not hidden
+
+    dirs = ['doc', 'exploratory', 'lib', 'products', 'src', 'tests']
+    assert all((path / dir_).exists() for dir_ in dirs)
