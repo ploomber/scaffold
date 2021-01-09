e99f1ef335b18e8c5688c1145af56523309b0a8c
Eduardo Blancas Reyes <github@blancas.io>
2021-01-08 18:52:20 -0600

    creating parent product folder if it doesnt exist

--- src/ploomber_scaffold/template/src/package_name/tasks/raw.py
+++ src/ploomber_scaffold/template/src/package_name/tasks/raw.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 
@@ -5,4 +7,5 @@ import pandas as pd
 def get(product):
     df = pd.DataFrame(np.random.rand(100, 2), columns=['a', 'b'])
     df.loc[:10, 'a'] = np.nan
+    Path(str(product)).parent.mkdir(parents=True, exist_ok=True)
     df.to_parquet(str(product))
