25544482b565c30772a2402aa8b0ed1f42f6201d
Eduardo Blancas Reyes <github@blancas.io>
2021-01-18 11:10:34 -0500

    adding extra versioneer test

--- tests/test_versioneer.py
+++ tests/test_versioneer.py
@@ -41,6 +41,7 @@ def test_release_version(move_to_project):
 @pytest.mark.parametrize('version, version_new', [
     ['0.1', '0.1.1dev'],
     ['0.1.1', '0.1.2dev'],
+    ['0.9', '0.9.1dev'],
 ])
 def test_bump_up_version(monkeypatch, version, version_new, move_to_project):
     monkeypatch.setattr(Versioner, 'current_version', lambda self: version)
